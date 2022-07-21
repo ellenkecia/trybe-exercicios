aaeac704a8feb99db61376df10391d9d768b4a8a
ellenkecia <ellenkeciaas@gmail.com>
2022-07-21 18:20:17 -0300

    primeiro exercicio

---
+++ fundamentos/bloco04-introducao-JS-e-logica-de-programacao/exercicio-4.1.js
@@ -0,0 +1,14 @@
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+console.log(tasksList.length);
+// 3
+
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+let searchForFirstTask = tasksList[0];
+console.log(searchForFirstTask);
+// Tomar café
+
+let searchForLastTask = tasksList[tasksList.length - 1];
+console.log(searchForLastTask);
+// Brincar com o cachorro
