7e17aa50d522919d669d7589c44c2f7d1506618f
ellenkecia <ellenkeciaas@gmail.com>
2022-07-21 19:25:02 -0300

    execucao requisito 03  exercicio final

--- fundamentos/bloco04-introducao-JS-e-logica-de-programacao/exercicio-final.js
+++ fundamentos/bloco04-introducao-JS-e-logica-de-programacao/exercicio-final.js
@@ -1,11 +1,15 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
+let media = 0.0;
 
-// console.log(numbers);
+console.log(numbers);
 
 for (let index = 0; index < numbers.length; index++) {
   soma += numbers[index];
 }
-console.log(soma)
+console.log(soma);
 
+  media = soma/numbers.length;
 
+  console.log(media);
+  
