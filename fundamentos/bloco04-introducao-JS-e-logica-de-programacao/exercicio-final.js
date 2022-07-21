c64d6a54f48469118c38ebfc42ad8c137b8d4e80
ellenkecia <ellenkeciaas@gmail.com>
2022-07-21 19:12:24 -0300

    execução requisito 02

---
+++ fundamentos/bloco04-introducao-JS-e-logica-de-programacao/exercicio-final.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+// console.log(numbers);
+
+for (let index = 0; index < numbers.length; index++) {
+  soma += numbers[index];
+}
+console.log(soma)
+
+
