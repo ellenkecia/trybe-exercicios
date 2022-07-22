31642b533d5c61d048cf45cf3f7896495bde3938
ellenkecia <ellenkeciaas@gmail.com>
2022-07-22 14:07:24 -0300

    execucao exercicio 05

--- fundamentos/bloco04-introducao-JS-e-logica-de-programacao/exercicio-final.js
+++ fundamentos/bloco04-introducao-JS-e-logica-de-programacao/exercicio-final.js
@@ -1,6 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 let media = 0.0;
+let maiorNumero = 0;
 
 console.log(numbers);
 
@@ -9,12 +10,20 @@ for (let index = 0; index < numbers.length; index++) {
 }
 console.log(soma);
 
+// exercicio  03
   media = soma/numbers.length;
 
   console.log(media);
-
+// exercicio 04
   if (media>20) {
     console.log('valor maior que 20');
   } else {
     console.log ('valor menor que 20')
   }
+
+for ( let index = 0 ; index < numbers.length; index++){
+  if (maiorNumero < numbers[index]){
+    maiorNumero = numbers[index];
+  }
+}
+console.log (maiorNumero);
