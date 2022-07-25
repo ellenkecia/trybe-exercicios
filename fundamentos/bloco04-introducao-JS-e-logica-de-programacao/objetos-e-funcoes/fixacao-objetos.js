06ffe84e51bdab5d7ebd9898f9be5c149eb2cd34
ellenkecia <ellenkeciaas@gmail.com>
2022-07-25 19:03:33 -0300

    exercicio 5

--- fundamentos/bloco04-introducao-JS-e-logica-de-programacao/objetos-e-funcoes/fixacao-objetos.js
+++ fundamentos/bloco04-introducao-JS-e-logica-de-programacao/objetos-e-funcoes/fixacao-objetos.js
@@ -15,3 +15,5 @@ player['bastInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018]
 console.table(player)
 
 console.log(' A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + player.bastInTheWorld.length + ' vezes')
+
+console.log ('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata');
