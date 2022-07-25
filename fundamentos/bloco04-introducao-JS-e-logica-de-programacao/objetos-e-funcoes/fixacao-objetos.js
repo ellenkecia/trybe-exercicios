069b18d784ddcae8b6afcf01089c8d31741bc3c0
ellenkecia <ellenkeciaas@gmail.com>
2022-07-25 16:00:00 -0300

    exercicio 3

---
+++ fundamentos/bloco04-introducao-JS-e-logica-de-programacao/objetos-e-funcoes/fixacao-objetos.js
@@ -0,0 +1,15 @@
+let player = {
+  name: 'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: {
+    golden: 2,
+    silver: 3,
+  }
+}
+
+console.log ( 'A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade')
+
+player[bastInTheWorld] = [2006, 2007, 2008, 2009, 2010, 2018]
+
+console.table (player)
