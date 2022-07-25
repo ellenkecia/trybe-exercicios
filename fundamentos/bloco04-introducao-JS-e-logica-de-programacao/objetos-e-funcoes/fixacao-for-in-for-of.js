542738c44e9635cd5c0c37dc94fecc30a98c9077
ellenkecia <ellenkeciaas@gmail.com>
2022-07-25 19:16:14 -0300

    exercicio de fixacao for in numero 2

---
+++ fundamentos/bloco04-introducao-JS-e-logica-de-programacao/objetos-e-funcoes/fixacao-for-in-for-of.js
@@ -0,0 +1,20 @@
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge',
+};
+
+for(let key in names){
+  console.log('Ola ' + names[key]);
+}
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+
+
+for(let key in car){
+  console.log(car[key]);
+}
