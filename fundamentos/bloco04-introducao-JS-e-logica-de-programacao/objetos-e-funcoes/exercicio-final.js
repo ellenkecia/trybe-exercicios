1a08b693744bd045b6d0f61502efbbb9b7ccab0e
ellenkecia <ellenkeciaas@gmail.com>
2022-07-25 22:02:54 -0300

    exercicio 3

--- fundamentos/bloco04-introducao-JS-e-logica-de-programacao/objetos-e-funcoes/exercicio-final.js
+++ fundamentos/bloco04-introducao-JS-e-logica-de-programacao/objetos-e-funcoes/exercicio-final.js
@@ -4,4 +4,26 @@ let info = {
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
-console.log ('Bem-vinda,' + ' ' + info.personagem)
+// 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console: Bem-vinda, Margarida
+
+console.log('Bem-vinda,' + ' ' + info.personagem);
+
+// 🚀 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console: 
+// {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   recorrente: 'Sim'
+// }
+
+info['recorrente'] = 'sim'
+
+console.log (info);
+
+// 3 - Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+
+for(let key in info){
+  console.log(info[key]);
+}
+
+
