e65ca8398b029448eaf8532bc0cd2e2f78865773
Ellen Kecia <ellenkeciaas@gmail.com>
2022-07-28 15:52:35 -0300

    primeiro exercicio

---
+++ fundamentos/bloco05-JS-DOM-Eventos-e-Web-Storage/5.1-DOM-e-seletores/script-seletores.js
@@ -0,0 +1,6 @@
+// Alterando descrição do id 
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+document.getElementById("page-title").innerText = "Meus filmes favoritos"
+document.getElementById("second-paragraph").innerText = "Todos os filmes que prendam minha atenção"
+document.getElementById("subtitle").innerText = "Minhas produtoras favoritas"
