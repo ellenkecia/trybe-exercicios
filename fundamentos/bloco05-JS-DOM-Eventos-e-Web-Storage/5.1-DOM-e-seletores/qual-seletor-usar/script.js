2e4573c8e4920a3b5bc1a5a60ac37a917e5805d4
Ellen Kecia <ellenkeciaas@gmail.com>
2022-07-28 20:09:13 -0300

    primeira atualizacao de backgroud

---
+++ fundamentos/bloco05-JS-DOM-Eventos-e-Web-Storage/5.1-DOM-e-seletores/qual-seletor-usar/script.js
@@ -0,0 +1,6 @@
+document.querySelector(".no-emergency-tasks").style.background = "yellow";
+
+document.querySelector(".emergency-tasks").style.background = "#ff9f84";
+
+document.querySelectorAll('h3').style.color = pink;
+
