47a76e2e4c78e882e0845c9998ce11875dc6b991
Ellen Kecia <ellenkeciaas@gmail.com>
2022-07-29 20:18:59 -0300

    execucao de penultimo  exercicio

--- fundamentos/bloco05-JS-DOM-Eventos-e-Web-Storage/5.1-DOM-e-seletores/exercicio-final-manipulacao-DOM/script.js
+++ fundamentos/bloco05-JS-DOM-Eventos-e-Web-Storage/5.1-DOM-e-seletores/exercicio-final-manipulacao-DOM/script.js
@@ -7,7 +7,10 @@ document.getElementsByTagName('p')[0].innerHTML = "Daqui dois anos estarei traba
 // Crie e execute uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
 document.getElementsByClassName('main-content')[0].style.background = 'rgb(76,164,109)';
 // Crie e execute uma função que mude a cor do quadrado vermelho para branco.
+document.getElementsByClassName('center-content')[0].style.background = 'rgb(255,255,255)';
 // Crie e execute uma função que corrija o texto da tag <h1>.
+document.getElementsByTagName('h1')[0].innerText = 'Exercício 5.1 - JavaScript';
 // Crie e execute uma função que modifique todo o texto da tag <p> para maiúsculo.
+document.getElementsByTagName('p')[0].textContent.toUpperCase()
 // Crie e execute uma função que exiba o conteúdo de todas as tags <p> no console.
 // */s
