4af3f044b17bbc483f68167cf0838e9d1ef1271d
Ellen Kecia <ellenkeciaas@gmail.com>
2022-07-29 18:52:07 -0300

    exercicio administrador do tempo Trybe

--- fundamentos/bloco05-JS-DOM-Eventos-e-Web-Storage/5.1-DOM-e-seletores/qual-seletor-usar/script.js
+++ fundamentos/bloco05-JS-DOM-Eventos-e-Web-Storage/5.1-DOM-e-seletores/qual-seletor-usar/script.js
@@ -1,6 +1,19 @@
 document.querySelector(".no-emergency-tasks").style.background = "yellow";
 
 document.querySelector(".emergency-tasks").style.background = "#ff9f84";
+const fundoPrimeiroH3 = document.querySelectorAll(".emergency-tasks h3");
+fundoPrimeiroH3.forEach(element => {
+    element.style.background = "#a500f3";
+});
 
-document.querySelectorAll('h3').style.color = pink;
+document.querySelector(".no-emergency-tasks").style.background = "#black";
+const fundoSegundoH3 = document.querySelectorAll(".no-emergency-tasks h3");
+fundoSegundoH3.forEach(element => {
+    element.style.background = "black";
+});
+
+document.querySelector("#header-container").style.background = "#00b069"
+
+document.querySelector("#footer-container").style.background = "#003533"
+
 
