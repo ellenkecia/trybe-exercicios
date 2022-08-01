ed9c238b07ec5d19f0808d94ac0a624eec1a59b6
Ellen Kecia <ellenkeciaas@gmail.com>
2022-08-01 19:01:33 -0300

    exercicio 1

---
+++ fundamentos/bloco05-JS-DOM-Eventos-e-Web-Storage/5.2-trabalhando-com-elementos/buscando-elementos/manipulacao.js
@@ -0,0 +1,10 @@
+// 1 Acesse o elemento elementoOndeVoceEsta.
+const elementoOndeVoceEsta = document.getElementById ('elementoOndeVoceEsta');
+
+// 2 Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+// 3 Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+// 4 Acesse o primeiroFilho a partir de pai.
+// 5 Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
+// 6 Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
+// 7 Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
+// 8 Agora acesse o terceiroFilho a partir de pai.
