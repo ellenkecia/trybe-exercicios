75653422599c0cfa8259eb3c1efc12fe142edc17
Ellen Kecia <ellenkeciaas@gmail.com>
2022-08-15 13:49:24 -0300

    segundo exercicio

--- fundamentos/bloco07-introducao-JS-ES6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte-I/script.js
+++ fundamentos/bloco07-introducao-JS-ES6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte-I/script.js
@@ -19,3 +19,17 @@ testingScope(true);
 
 const novafuncao = (escopo) => escopo === true ? `Não devo ser utilizada fora do meu escopo (if)` : `Não devo ser utilizada fora meu escopo (else)`;
 console.log(novafuncao(false));
+
+// 🚀 Crie uma função que retorne um array em ordem crescente.
+// Crie uma função que faça com que o array oddsAndEvens fique em ordem crescente;
+// Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne a frase "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+// Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort(). Spoiler: É possível realizar uma função que ordene qualquer array de números.
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens = (constanteParaOrdernar)=> {
+  const constanteOrdenada = constanteParaOrdernar.sort((a, b) => a - b);
+  return constanteOrdenada;
+};
+
+console.log(sortOddsAndEvens(oddsAndEvens));
