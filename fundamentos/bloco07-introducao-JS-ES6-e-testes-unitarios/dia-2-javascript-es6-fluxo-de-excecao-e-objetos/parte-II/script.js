bc0dfa7adbc7693c7737f49a58e88a9beb7d0a02
Ellen Kecia <ellenkeciaas@gmail.com>
2022-08-24 18:09:52 -0300

    alteracoes exercicios parte II

--- fundamentos/bloco07-introducao-JS-ES6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/parte-II/script.js
+++ fundamentos/bloco07-introducao-JS-ES6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/parte-II/script.js
@@ -33,17 +33,45 @@ const order = {
       total: 60,
     },
   };
-  
+
+// Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
+// Note que o parâmetro da função já está sendo passado na chamada da função.
+ 
   const customerInfo = (order) => {
-    // Adicione abaixo as informações necessárias.
-  
+    const customerInfo = (order) => {
+        const address = 'address'; // Armazenando a palavra 'address' em uma variável
+        const deliveryPerson = order.order.delivery.deliveryPerson; // Acesse e armazene a chave `deliveryPerson` em uma variável através de 'notação de ponto'.
+        const customerName = order['name']; // Acesse e armazene a chave `name` em uma variável através de 'notação de chaves'.
+        const customerPhone = order['phoneNumber']; // Acesse e armazene a chave `phoneNumber` em uma variável através de 'notação de chaves'.
+        const street = order[address].street; // Acesse e armazene a chave `street` em uma variável, através de 'notação de chaves' e 'notação de ponto'.
+        const number = order[address].number; // Acesse e armazene a chave `number` em uma variável, através de 'notação de chaves' e 'notação de ponto'.
+        const apartment = order[address].apartment; // Acesse e armazene a chave `apartment` em uma variável, através de 'notação de chaves' e 'notação de ponto'.
+      
+        console.log(`Olá ${deliveryPerson}, entrega para: ${customerName}, Telefone: ${customerPhone}, R. ${street}, Nº: ${number}, AP: ${apartment}`);
+      }
+      
+      customerInfo(order);
   }
   
   customerInfo(order);
-  
+
+// Complete a função orderModifier() para que seu retorno seja similar a "Olá Luiz Silva, o total do seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00."
+// Modifique o nome da pessoa compradora.
+// Modifique o valor total da compra para R$ 50,00.
+
   const orderModifier = (order) => {
-    // Adicione abaixo as informações necessárias.
-  
+    const novoNome = order.name = 'Luiz Silva';
+    //const newBuyer = order.name = 'Luiz Silva';
+    const pedidoPizza = Object.keys(order.order.pizza);
+    //const pizzas = Object.keys(order.order.pizza);
+    const valorFinal = order.payment.total = '50';
+    //const newTotal = order.payment.total = '50';
+    const bebidaCoca = order.order.drinks.coke.type;
+    //const drinks = order.order.drinks.coke.type;
+
+    console.log(`Olá ${novoNome}, o total do seu pedido de ${pedidoPizza[0]}, ${pedidoPizza[1]} e ${bebidaCoca} é R$ ${valorFinal},00.`);
   }
   
   orderModifier(order);
+  
+  
