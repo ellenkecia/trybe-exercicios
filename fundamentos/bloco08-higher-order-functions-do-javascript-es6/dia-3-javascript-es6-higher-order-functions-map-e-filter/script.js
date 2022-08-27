c445e72c62a736ac1eda31a179c028364c55ed9f
Ellen Kecia <ellenkeciaas@gmail.com>
2022-08-27 15:18:26 -0300

    exercicios 1 a 3 do dia 8.3

--- fundamentos/bloco08-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/script.js
+++ fundamentos/bloco08-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/script.js
@@ -66,12 +66,27 @@ const books = [
 
 //   🚀 1 - Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
 // Dica: Use a função map
+const newbooks = books.map((dadosLivros) =>`${dadosLivros.name} - ${dadosLivros.genre} - ${dadosLivros.author.name}`);
+console.log(newbooks)
 
-// 🚀 2 - Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author, com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+// 🚀 2 - Construa um array de objetos a partir do array de livros. 
+// Cada objeto deve conter uma propriedade author, com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. 
+// O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
 // Dica: use as funções map, sort
+
+const books2 = books.map((dadosLivros)=> (
+  {
+    author: dadosLivros.author.name,
+    age: dadosLivros.releaseYear - dadosLivros.author.birthYear,
+  })).sort((a , b) => a.age - b.age)
+
+console.log(books2);
 
 // 🚀 3 - Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
 // Dica: use a função filter;
+
+const genreFictionOrFantasy = books.filter((genreFilter) => genreFilter.genre === 'Ficção Científica' || genreFilter.genre === 'Fantasia');
+console.log(genreFictionOrFantasy);
 
 // 🚀 4 - Crie um array formado pelos livros com mais de 60 anos desde sua publicação. Esse array deve ser ordenado do livro mais velho ao mais novo.
 // Dica: use as funções filter e sort
