65971dcd978db6767fffa4f3f9f1bdd76c599fa5
Ellen Kecia <ellenkeciaas@gmail.com>
2022-10-17 19:31:26 -0300

    inserindo botões e testando se sao localizados

--- front-end/bloco14-testes-automatizados-com-RTL/RTL-primeiros-passos/testes-react/src/App.js
+++ front-end/bloco14-testes-automatizados-com-RTL/RTL-primeiros-passos/testes-react/src/App.js
@@ -9,6 +9,8 @@ function App() {
         Email
         <input id="id-email" type="email" />
       </label>
+      <input id="btn-send" type="button" value="Enviar" />
+      <input id="btn-back" type="button" value="Voltar" />
     </div>
   );
 }
--- front-end/bloco14-testes-automatizados-com-RTL/RTL-primeiros-passos/testes-react/src/App.test.js
+++ front-end/bloco14-testes-automatizados-com-RTL/RTL-primeiros-passos/testes-react/src/App.test.js
@@ -5,5 +5,18 @@ test('Verifica se existe o campo Email', () => {
   render(<App />);
   const inputEmail = screen.getByLabelText('Email');
   expect(inputEmail).toBeInTheDocument();
-  expect(inputEmail).toHaveProperty('type', 'email')
+  expect(inputEmail).toHaveProperty('type', 'email');
+});
+
+
+//Como foi inserido mais um botão, a função abaixo já não funciona
+// test('Verificando se existe um botão', () => {
+//   render(<App/>)
+//   const btn = screen.getByRole('button');
+//   expect(btn).toBeInTheDocument();
+// });
+test('Verificando se existem dois botões', () => {
+  render(<App />);
+  const buttons = screen.getAllByRole('button');
+  expect(buttons).toHaveLength(2);
 });
